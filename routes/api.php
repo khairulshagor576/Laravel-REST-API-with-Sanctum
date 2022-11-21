a62ca3ad8fbe2774fbbf12a4fdc0e97b52c40a56
feroz <ferozcse012@gmail.com>
2022-11-21 13:36:44 +0600

    laravel api

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Public Api
+// Route::get('/students',[StudentController::class,'index']);
+// Route::get('/student/{id}',[StudentController::class,'show']);
+// Route::post('/students',[StudentController::class,'store']);
+// Route::put('/student/{id}',[StudentController::class,'update']);
+// Route::delete('/student/{id}',[StudentController::class,'destroy']);
+// Route::get('/student/search/{city}',[StudentController::class,'search']);
+
+   Route::post('/register',[UserController::class,'register']);
+
+//Protected Api
+
+   Route::middleware('auth:sanctum')->get('/students',[StudentController::class,'index']);
+
+
+
