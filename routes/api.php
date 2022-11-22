1bc0b7527dafb3fdbe58b2bba4e320ca86fe1d27
feroz <ferozcse012@gmail.com>
2022-11-22 14:28:30 +0600

    specific route under authentication

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //Public Api
-// Route::get('/students',[StudentController::class,'index']);
-// Route::get('/student/{id}',[StudentController::class,'show']);
+//Route::get('/students',[StudentController::class,'index']);
+Route::get('/student/{id}',[StudentController::class,'show']);
 // Route::post('/students',[StudentController::class,'store']);
 // Route::put('/student/{id}',[StudentController::class,'update']);
 // Route::delete('/student/{id}',[StudentController::class,'destroy']);
 // Route::get('/student/search/{city}',[StudentController::class,'search']);
 
-   Route::post('/register',[UserController::class,'register']);
+ Route::post('/register',[UserController::class,'register']);
 
 //Protected Api
 
-   Route::middleware('auth:sanctum')->get('/students',[StudentController::class,'index']);
+  Route::middleware('auth:sanctum')->get('/students',[StudentController::class,'index']);
+  Route::middleware('auth:sanctum')->get('/student/{id}',[StudentController::class,'show']);
 
 
 
